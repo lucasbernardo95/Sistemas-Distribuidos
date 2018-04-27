000b032809ed9f5cbaf09c8feee07bbfa9e201a6
Lucas <lucasbernardo95@gmail.com>
2018-04-27 09:21:43 -0300

    Monitoramento de pulsos
    Monitoramento de pulsos de sinal no arduino

---
+++ RPM com pulsein.ino
@@ -0,0 +1,23 @@
+int pin = 2; //Entrada do sinal
+unsigned long duration;
+double aux = 0; //Usada para fazer o cálculo da frequência.
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pin, INPUT);
+}
+
+void loop() {
+  duration = pulseIn(pin, HIGH);
+  aux = duration; //Atribui a aux o tempo que o sinal passou em nível alto (em microssegundos).
+  aux = 500000/aux; // Como o pulsein mede apenas o tempo em nível alto e o valor está em microssegundos, dividor 500000 por pelo returno do pulsein nos dará a frequência.
+  Serial.print(duration); //Imprime o tempo do sinal em nível alto.
+  Serial.print(" | ");
+  Serial.print(aux); //Imprime a frequência em Hz. Dois pulsos.
+  //delay(50);
+  Serial.print(" | ");
+  Serial.print((aux/2)*60);
+  Serial.println(" RPM");
+  delay(100);
+  
+}
